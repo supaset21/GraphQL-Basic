7b7d602bd8637196e18d6d3094f4819aac2897ad
Supaset <supaset21@hotmail.com>
2019-07-11 12:14:16 +0700

    basic condition

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-##GraphQL Basic use
+## GraphQL Basic use
 
 ```
 npm install
 ```
+
+[อ่านบทความ](https://wiki.bridgeasia.me/post/software/graphql/)
--- server.js
+++ server.js
@@ -4,17 +4,39 @@ const app = express();
 const graphqlMiddleware = require("express-graphql");
 const { graphql, buildSchema } = require("graphql");
 
+const customers = require("./data");
+
 const port = 3000;
+
+// const schema = buildSchema(`
+//   type Query {
+//     hello : String,
+//     world : String
+//   }
+
+//   type Customer {
+//     firstName : String
+//     lastName : String
+//   }
+// `);
 
 const schema = buildSchema(`
   type Query {
-    hello : String,
-    world : String
+    customer(id : ID!) : Customer
+    customers(limit : Int = 1 , gender : String , age : AGE) : [Customer]
   }
 
   type Customer {
+    id : Int
     firstName : String
     lastName : String
+    age : Int
+    gender : String
+  }
+
+  enum AGE {
+    YOUNG
+    OLD
   }
 `);
 
@@ -24,6 +46,27 @@ const resolver = {
   },
   world() {
     return "world";
+  },
+  customer(args) {
+    return customers.find(c => c.id == args.id);
+  },
+  customers(args) {
+    console.log(args);
+
+    // let result = customers;
+    let result = [].concat(customers);
+
+    if (args.gender) {
+      result = result.filter(c => c.gender == args.gender);
+    }
+
+    if (args.age) {
+      result =
+        args.age == "YOUNG"
+          ? result.filter(c => c.age <= 25)
+          : result.filter(c => c.age > 25);
+    }
+    return result.slice(0, args.limit);
   }
 };
 
